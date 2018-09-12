9d89e7c100b78f0f05b8c7d94e127493aef0fe92
mihai511 <32911005+mihai511@users.noreply.github.com>
2018-09-12 21:27:11 +0300

    Update functions.h

--- functions.h
+++ functions.h
@@ -2,3 +2,5 @@ struct min_heap* min_heap_tree(struct min_heap* root, int arr[], int i, int n);
 void ordering_array(int arr[], int i, int n);
 void create_min_heap_array(int arr[], int n);
 void search_min_heap(struct min_heap *root, int searched_value, int *found);
+int get_position_of_element(int arr[], int n, int searched_value);
+void pop_element_array(int arr[], int *n, int i);
